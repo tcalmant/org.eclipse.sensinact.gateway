b64f25b4d6953509bac0a48315437344583f36c1
Tim Ward <timothyjward@apache.org>
2023-02-23 18:50:14 +0000

    Make the Web Socket Servlet start cleanly on first use
    Signed-off-by: Tim Ward <timothyjward@apache.org>

--- prototype/northbound/websocket/src/main/java/org/eclipse/sensinact/northbound/ws/impl/WebSocketJettyRegistrar.java
+++ prototype/northbound/websocket/src/main/java/org/eclipse/sensinact/northbound/ws/impl/WebSocketJettyRegistrar.java
@@ -12,8 +12,18 @@
 **********************************************************************/
 package org.eclipse.sensinact.northbound.ws.impl;
 
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.websocket.core.WebSocketComponents;
+import org.eclipse.jetty.websocket.core.server.WebSocketServerComponents;
+import org.eclipse.jetty.websocket.server.JettyWebSocketServerContainer;
 import org.eclipse.jetty.websocket.server.JettyWebSocketServlet;
 import org.eclipse.jetty.websocket.server.JettyWebSocketServletFactory;
+import org.eclipse.jetty.websocket.server.config.JettyWebSocketServletContainerInitializer;
 import org.eclipse.sensinact.prototype.SensiNactSessionManager;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -22,6 +32,10 @@ import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardServletAsync
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardServletPattern;
 
 import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 
 @Component(service = Servlet.class, scope = ServiceScope.PROTOTYPE)
 @HttpWhiteboardServletPattern("/ws")
@@ -32,6 +46,43 @@ public class WebSocketJettyRegistrar extends JettyWebSocketServlet {
 
     @Reference
     SensiNactSessionManager sessionManager;
+
+    private final AtomicBoolean initCalled = new AtomicBoolean(false);
+    private final CountDownLatch initComplete = new CountDownLatch(1);
+
+    @Override
+    public void init() throws ServletException {
+        // Deliberately block initialization. This is needed as there is
+        // no Jetty context on the thread until a request occurs
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        // Check to see if init needs calling
+        if (initCalled.getAndSet(true)) {
+            // Someone else is responsible, wait until they are done
+            try {
+                initComplete.await();
+            } catch (InterruptedException e) {
+                throw new ServletException(e);
+            }
+        } else {
+            // Initialise now
+            try {
+                ServletContext servletContext = getServletContext();
+                ServletContextHandler contextHandler = ServletContextHandler.getServletContextHandler(servletContext,
+                        "Jetty WebSocket init");
+                WebSocketServerComponents.ensureWebSocketComponents(contextHandler.getServer(), servletContext);
+                JettyWebSocketServerContainer.ensureContainer(servletContext);
+                super.init();
+            } finally {
+                // Tell other callers that they can stop waiting
+                initComplete.countDown();
+            }
+        }
+        // Normal service resumes
+        super.service(req, res);
+    }
 
     @Override
     protected void configure(JettyWebSocketServletFactory factory) {
